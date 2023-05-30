5a4d52d0bff6985ca609651bdd51aeff3d6206c0
[Muniru Issah] <[imuniru@st.ug.edu.gh]>
2023-05-30 12:15:18 +0000

    Add Login Page

---
+++ src/types/schemas.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+export const loginSchema = yup
+  .object({
+    email: yup.string().required(),
+    password: yup.string().required(),
+  })
+  .required();
+
+export type LoginFormData = yup.InferType<typeof loginSchema>;
